3233fd32af301fd97b613c89a0c7417c53038df7
triggerThis <2126384@qq.com>
2016-02-01 15:07:24 +0800

    for uniqueidentifier id and domain jar

---
+++ src/main/java/OT/JF/Domain/User.java
@@ -0,0 +1,58 @@
+package OT.JF.Domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "users, org, role, department, permission, dataDictionary")
+@Data
+@EqualsAndHashCode(exclude = { "id" })
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	// if using uniqueidentifier
+	//@Id
+    //@Column(columnDefinition="uniqueidentifier")
+    //GeneratedValue(strategy = GenerationType.AUTO)
+    //private String id = UUID.randomUUID().toString();
+    //private UUID id = UUID.randomUUID();    //实例化实体类的时候初始化id
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	@Column(name = "code", nullable = false)
+	private Integer code;
+
+	@Column(name = "quantity", nullable = false)
+	private Integer quantity;
+}
